a901e9329e967770f866e5984274a11c445b5bd6
SuperCoder <supercoder@SuperCoders-Air.fritz.box>
2022-01-30 21:18:57 +0100

    new

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var nam = document.getElementById("nam");
+var email = document.getElementById("email");
+var your = document.getElementById("your");
+var send = document.getElementById("send");
+var ne = document.getElementById("ne");
+function subscribe() {
+    console.log(nam.value);
+    console.log(email.value);
+    console.log(your.value);
+    console.log(ne.checked);
+}
